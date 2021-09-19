e0e9e44e6cbb138b983c179e4c1cc7bbdffdb63b
Siddharth <iamsiddharthpathak@gmail.com>
2021-09-20 01:32:04 +0530

    fcntl issue

--- weatherApp/wsgi.py
+++ weatherApp/wsgi.py
@@ -6,9 +6,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
-
-import os
 from dj_static import Cling
+import os
 
 from django.core.wsgi import get_wsgi_application
 
